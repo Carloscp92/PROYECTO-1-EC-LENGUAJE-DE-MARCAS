3cbb62e376e14ca68eb79fda420ff311a1624648
Carloscp92 <carmor.cpc92@gmail.com>
2025-08-11 21:07:12 +0200

    creacion de popup y pagina de gestion de privacidad

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,23 @@
+//===MENU BURGUER===
+
 let burguer= document.querySelector(".burguer");
 let nav= document.querySelector("nav");
 
 burguer.addEventListener("click",()=>{
 
    nav.classList.toggle("visible");
+})
+
+//===POPUP COOKIES===
+
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies")
+
+if(!localStorage.cookiesAccepted){
+popup.classList.add("show");
+}
+
+btn.addEventListener("click",function(){
+   localStorage.cookiesAccepted="true";
+   popup.classList.remove("show");
 })
